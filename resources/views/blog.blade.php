8b4855d25c935040d6f05facff69efe471535be5
yamiranea <yamilarenea@gmail.com>
2024-02-12 23:19:37 +0100

    adding sections to views

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    @include('partials.navigation')
+@section('content')
     <h1>Blog</h1>
-</body>
-
-</html>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    @include('partials.navigation')
-    <h1>INICIO</h1>
-</body>
-
-</html>
+@section('content')
+    <h1>Home</h1>
+@endsection
